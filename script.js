11e4cd7ddf53871eb050b15d6e6b15a7fd29a762
Tuan Ha <hatuan@email.cz>
2023-10-29 21:58:58 +0100

    add a function to create a grid instead of loop, add css to it

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const gridContainer = document.querySelector(".grid-container");
+const buttonSize = document.querySelector(".grid-size");
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridContainer.appendChild(gridItem);
+//initial size of the grid
+let gridSize = 16;
+
+//function that creates a grid + calcute size to fit and add the css
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridContainer.appendChild(gridItem);
+
+        gridItem.style.flex = `1 0 calc(100% / ${gridSize})`;
+    }
 }
 
-const buttons = document.querySelectorAll(".grid-item");
+createGrid(gridSize);
 
-buttons.forEach(button => {
-    button.addEventListener("mouseenter", () => {
-        button.style.backgroundColor = "black";
+//for each square in grid change color when hovered over
+const grids = document.querySelectorAll(".grid-item");
+grids.forEach(grid => {
+    grid.addEventListener("mouseenter", () => {
+        grid.style.backgroundColor = "black";
     });
-});
+}); 
