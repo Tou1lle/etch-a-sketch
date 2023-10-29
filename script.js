8bf7ec61100db8ad9c9a1ec506304e9c07d0d11c
Tuan Ha <hatuan@email.cz>
2023-10-29 21:37:04 +0100

    add a button

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const container = document.querySelector(".container");
+const gridContainer = document.querySelector(".grid-container");
 
 for (let i = 0; i < 16 * 16; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
-    container.appendChild(gridItem);
+    gridContainer.appendChild(gridItem);
 }
+
+const buttons = document.querySelectorAll(".grid-item");
+
+buttons.forEach(button => {
+    button.addEventListener("mouseenter", () => {
+        button.style.backgroundColor = "black";
+    });
+});
